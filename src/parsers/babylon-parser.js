00a1c1b55f3694f426b55f8fb39db27313c96977
James Sear <jpsear@gmail.com>
2021-03-26 16:10:19 +0000

    Update babylon-parser.js

--- src/parsers/babylon-parser.js
+++ src/parsers/babylon-parser.js
@@ -3,6 +3,7 @@ const babylon = require('@babel/parser')
 module.exports = (type, plugins) => input =>
   babylon.parse(input, {
     sourceType: 'module',
+    allowImportExportEverywhere: true,
     plugins: [type].concat(
       plugins || [
         'jsx',
